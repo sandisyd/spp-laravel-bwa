33e6f204454096bc3972e4c5688ed3095b69b945
Sandi <sandidwi367@gmail.com>
2025-04-11 08:00:57 +0700

    new project

---
+++ app/Models/Transaction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+
+    public function users()
+    {
+        return $this->belongsTo(User::class, "user_id");
+    }
+
+    public function departments(){
+        return $this->belongsTo(Department::class, "department_id");
+    }
+}
